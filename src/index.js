e08fe424f94ec8711a6c660412d5b10fe2962d2e
jxeong <jxeong@gmail.com>
2024-09-04 22:30:21 +0900

    22:30 commit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
 );
 
 reportWebVitals();
